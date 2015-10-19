11824de1356bb52d16d8367f76079691d4ae96ee
Donn Felker <donn@donnfelker.com>
2015-10-19 10:15:29 -0400

    Oops, forgot to add views

---
+++ app/src/main/java/com/donnfelker/tasko/MainFragment.java
@@ -0,0 +1,41 @@
+package com.donnfelker.tasko;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.Bind;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainFragment extends Fragment {
+
+    @Bind(R.id.main_task_list) protected RecyclerView taskList;
+
+    public MainFragment() {
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_main, menu);
+    }
+}
